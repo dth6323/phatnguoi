8a43259231da9b690179d345e44a8de8fa1c6a0d
dth63 <dth6323@gmail.com>
2024-11-17 22:24:20 +0700

    up db and crud

--- src/main/webapp/app/entities/entity.routes.ts
+++ src/main/webapp/app/entities/entity.routes.ts
@@ -6,6 +6,26 @@ const routes: Routes = [
     data: { pageTitle: 'phatnguoiApp.adminAuthority.home.title' },
     loadChildren: () => import('./admin/authority/authority.routes'),
   },
+  {
+    path: 'cccd',
+    data: { pageTitle: 'phatnguoiApp.cCCD.home.title' },
+    loadChildren: () => import('./cccd/cccd.routes'),
+  },
+  {
+    path: 'type-violation',
+    data: { pageTitle: 'phatnguoiApp.typeViolation.home.title' },
+    loadChildren: () => import('./type-violation/type-violation.routes'),
+  },
+  {
+    path: 'vehicle-registrations',
+    data: { pageTitle: 'phatnguoiApp.vehicleRegistrations.home.title' },
+    loadChildren: () => import('./vehicle-registrations/vehicle-registrations.routes'),
+  },
+  {
+    path: 'violations',
+    data: { pageTitle: 'phatnguoiApp.violations.home.title' },
+    loadChildren: () => import('./violations/violations.routes'),
+  },
   /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
 ];
 
